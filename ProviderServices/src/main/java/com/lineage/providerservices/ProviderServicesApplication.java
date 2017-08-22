93fcf23771136f470e7679c431601d3f0ed3f522
Lineage Software <lineagesoftware@gmail.com>
2017-08-21 23:47:04 -0400

    War file creation.

--- ProviderServices/src/main/java/com/lineage/providerservices/ProviderServicesApplication.java
+++ ProviderServices/src/main/java/com/lineage/providerservices/ProviderServicesApplication.java
@@ -2,11 +2,19 @@ package com.lineage.providerservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProviderServicesApplication {
+public class ProviderServicesApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProviderServicesApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProviderServicesApplication.class, args);
 	}
+
 }
